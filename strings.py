9f1a40f3a05a9fa0073892fbf20d32cdf8e6ae57
LLLP7 <121124279+LLLP7@users.noreply.github.com>
2023-03-16 03:43:46 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -50,4 +50,4 @@ If Child mode is enabled, akinator won't show any NSFW content.
 <b>Current Status :</b> <pre>Child mode is {} !</pre>
 """
 
-AKI_FIRST_QUESTION = "This is the first question. You can't go back any further!"
+AKI_FIRST_QUESTION = "هذا أول سؤال لايمكنك الرجوع"
