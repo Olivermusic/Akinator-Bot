c870ab7b0d4fd118271b8acc04039933dd4f4246
Olivermusic <121124279+Olivermusic@users.noreply.github.com>
2024-04-17 04:34:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-AKI_MONGO_HOST = os.environ.get('aki_mongo_host', "")
-BOT_TOKEN = os.environ.get('bot_token', "")
+AKI_MONGO_HOST = os.environ.get('aki_mongo_host', "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
+BOT_TOKEN = os.environ.get('bot_token', "5871718163:AAEKVvHBTTYURGJsBEIpcCiSx0c1bNAk2PY")
